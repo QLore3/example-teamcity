363503a05cc81c4a53139a8d32a5a13d653afdfb
root <root@DESKTOP-V6SV8AO.localdomain>
2026-05-18 21:02:21 +0300

    added hunter task

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -6,6 +6,9 @@ public class Welcomer{
 	public String sayWelcome() {
 		return "Welcome home, good hunter. What is it your desire?";
 	}
+	public String sayHunter() {
+	    return "something hunter duck";
+	}
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
